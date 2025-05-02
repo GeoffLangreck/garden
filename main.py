27cce1e7a389ea7b41f3c26e17b9d3da86ec27bf
GeoffLangreck <141423373+GeoffLangreck@users.noreply.github.com>
2025-05-01 21:28:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
-from Garden_data import load_garden_from_file, save_garden_to_file
-from Garden_menus import handle_menu_choice
+import garden_data
+import garden_menus
+import default
 
 
 
@@ -10,35 +10,10 @@ from Garden_menus import handle_menu_choice
 # -------------------------------
 
 if __name__ == "__main__":
-    garden = load_garden_from_file()
+    garden = garden_data.load_garden_from_file()
     if not garden:
         # Default garden data (used if no file exists or file is empty)
-        garden = {
-        "North Zone": {
-            "Pine": {
-                "type": "tree",
-                "height_ft": 40,
-                 "main_color": "brown"
-            },
-            "Cactus": {
-                "type": "flowering plant",
-                "height_ft": 6,
-                "main_color": "green"
-            }
-        },
-        "West Zone": {
-            "Rubber Tree": {
-                "type": "tree",
-                "height_ft": 12,
-                "main_color": "beige"
-            },
-            "Diamicy": {
-                "type": "flower",
-                "height_ft": 1,
-                "main_color": "red"
-            }
-        }
-    }
+        garden = default.default_garden
 
-    handle_menu_choice(garden)
-    save_garden_to_file(garden)
+    garden_menus.handle_menu_choice(garden)
+    garden_data.save_garden_to_file(garden)
