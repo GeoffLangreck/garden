3c748ac9d04f36a0f67730e4331dfb752b22e45e
GeoffLangreck <141423373+GeoffLangreck@users.noreply.github.com>
2025-05-01 21:29:15 -0500

    Update and rename Garden_menus.py to garden_menus.py

--- garden_menus.py
+++ garden_menus.py
@@ -1,15 +1,20 @@
-from Garden_core import view_garden_options, add_plant, update_plant_color, update_plant_height, add_zone
+import garden_core
 
 # -------------------------------
 # Display the main menu
 # -------------------------------
 def print_main_menu():
-    print("1. View Garden.")
-    print("2. Update Plant Color.")
-    print("3. Update Plant Height.")
-    print("4. Add Zone.")
-    print("5. Add Plant.")
+    print("Garden Viewer")
+    print("1. View Garden")
+    print("2. Update Plant Color")
+    print("3. Update Plant Height")
+    print()
+    print("Zone & Plant Management")
+    print("4. Add Zone")
+    print("5. Add Plant")
+    print()
     print("6. Exit")
+
 
 # -------------------------------
 # Handle main menu choices
@@ -18,24 +23,24 @@ def handle_menu_choice(garden):
     while True:
         try:
             print_main_menu()
-            choice = input("Enter your choice (1-4): ")
+            choice = input("Enter your choice (1-6): ")
 
             if choice == "1":
-                view_garden_options(garden)
+                garden_core.view_garden_options(garden)
             elif choice == "2":
                 zone_name = input("Enter the zone name: ")
                 plant_name = input("Enter the plant name: ")
                 new_color = input("Enter new color: ")
-                update_plant_color(zone_name, plant_name, new_color)
+                garden_core.update_plant_color(zone_name, plant_name, new_color)
             elif choice == "3":
                 zone_name = input("Enter the zone name: ")
                 plant_name = input("Enter the plant name: ")
                 new_height = int(input("Enter new height: "))
-                update_plant_height(zone_name, plant_name, new_height)
+                garden_core.update_plant_height(zone_name, plant_name, new_height)
             elif choice == "4":
-                add_zone(garden)
+                garden_core.add_zone(garden)
             elif choice == "5":
-                add_plant(garden)
+                garden_core.add_plant(garden)
             elif choice == "6":
                 print("Exiting program.")
                 break
@@ -43,6 +48,7 @@ def handle_menu_choice(garden):
                 print("Invalid choice.")
         except Exception as e:
             print(f"Error: {e}")
+
 
 
 
